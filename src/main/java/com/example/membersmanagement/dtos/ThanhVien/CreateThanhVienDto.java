88671a1c30a943cc087fbf84ecfe5ec904d9d047
dannychtai <danny0110chitai@gmail.com>
2024-05-05 14:47:41 +0700

    fix member.html

--- src/main/java/com/example/membersmanagement/dtos/ThanhVien/CreateThanhVienDto.java
+++ src/main/java/com/example/membersmanagement/dtos/ThanhVien/CreateThanhVienDto.java
@@ -10,23 +10,27 @@ import jakarta.validation.constraints.Email;
 @Builder
 public class CreateThanhVienDto {
     @NotEmpty(message = "Mã thành viên không được để trống.")
-    @Size(min = 7, max = 7, message = "Mã thành viên phải 7 kí tự.")
+    @Size(min = 9, max = 9, message = "Mã thành viên phải 9 kí tự.")
     private String maTV;
 
+    @Size(min = 5, max = 30, message = "Họ tên phải từ 5 - 30 kí tự.")
     private String hoTen;
 
+    @NotEmpty(message = "Khoa không được trống.")
     private String khoa;
 
+    @NotEmpty(message = "Ngành không được trống.")
     private String nganh;
 
     @Size(min = 10, max = 11, message = "Số điện thoại không hợp lệ")
     private String sdt;
 
-    @Email(message = "Địa chỉ email không hợp lệ.")
+    @NotEmpty(message = "Email không được để trống.")
+    @Email(message = "Email không đúng định dạng.")
     private String email;
 
     @NotEmpty(message = "Mật khẩu không được để trống.")
-    @Size(max = 10, message = "Mật khẩu không được quá 10 kí tự.")
+    @Size(min = 6, max = 8, message = "Mật khẩu phải từ 6 - 8 kí tự.")
     private String password;
 
 }
