4a3e37286b04c69c058c88bc892a9f2bd15fbc77
Hoang Gia Bao <120194990+hgbaodev@users.noreply.github.com>
2024-05-07 15:14:07 +0700

    Update WebSecurityConfig.java

--- src/main/java/com/example/membersmanagement/config/WebSecurityConfig.java
+++ src/main/java/com/example/membersmanagement/config/WebSecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
