ceb178bf5909cd75065ef320ca45b124103db7f8
lequanphat <lequanphat3579@gmail.com>
2024-05-08 19:51:12 +0700

    change logo for forgot-password, checkemail

--- src/main/java/com/example/membersmanagement/config/CustomUserDetailsService.java
+++ src/main/java/com/example/membersmanagement/config/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.example.membersmanagement.config;
 
 import com.example.membersmanagement.entities.ThanhVienEntity;
 import com.example.membersmanagement.repositories.ThanhVienRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,9 +14,13 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private ThanhVienRepository thanhVienRepository;
 
+    @Autowired
+    private HttpSession session;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         ThanhVienEntity user = thanhVienRepository.findByEmail(username);
+        session.setAttribute("username", username);
         if (user == null) {
             try {
                 user = thanhVienRepository.findByMaTV(Integer.parseInt(username));
